a4ba7a03223893cba85e99c6cfdd331943c7a6e9
likhitaaluru <likhitaaluru@gmail.com>
2018-08-10 16:45:29 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,12 @@
 # dog.rb
+class Dog
+  attr_reader :name
+  attr_accessor :breed
+  def name=(name)
+    @name = name
+  end
+end
+
+fido = Dog.new 
+fido.name = "Fido"
+puts fido.name
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,12 @@
 # person.rb
+class Person
+  attr_reader :name
+  attr_accessor :job
+  def name=(name)
+    @name = name
+  end
+end
+
+joe = Person.new 
+joe.name = "Joe"
+puts joe.name
